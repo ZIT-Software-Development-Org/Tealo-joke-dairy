a81371fdb9aa3939e00cf48e7fbb0504e440750b
Willim Johnson <williamj2001tealo@gmail.com>
2025-03-07 12:34:35 +0000

    latest pust

--- Back-End/controllers/authController.js
+++ Back-End/controllers/authController.js
@@ -1,11 +1,11 @@
-import pool from "../db/db.js";
+import sequelize from "../config/database.js";
 import bcrypt from "bcrypt";
 
 export const createData = async (req, res) => {
   const { name, email, password } = req.body;
 
   try {
-    // Hash the password
+    //Hash the password
     const saltRounds = 10;
     const hashedPassword = await bcrypt.hash(password, saltRounds);
 
@@ -15,9 +15,11 @@ export const createData = async (req, res) => {
       VALUES ($1, $2, $3)
     `;
     const values = [name, email, hashedPassword];
-    await pool.query(query, values);
+    await sequelize.query(query, values);
 
     res.status(201).json({ success: true, message: "Data created successfully" });
+    console.log('Data created successfully')
+    
   } catch (error) {
     console.error("Failed to create data:", error);
     res.status(500).json({ success: false, message: "Failed to create data" });
